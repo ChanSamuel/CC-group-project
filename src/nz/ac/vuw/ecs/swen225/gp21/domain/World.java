2d415a2c77ef18bede6ce7f8171e90b263ccf7fd
BudgiePanic <bsanson90@gmail.com>
2021-08-25 16:27:22 +1200

    Created top level world class
    The world class is the high level class that manages the representation of the game.

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/World.java
@@ -0,0 +1,22 @@
+package nz.ac.vuw.ecs.swen225.gp21.domain;
+
+public class World {
+
+	public void moveUp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void moveDown() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void moveLeft() {
+		
+	}
+	
+	public void moveRight() {
+		
+	}
+}
