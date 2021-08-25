294e4357c56e62889266445f5f06dd0c99cd432b
BudgiePanic <bsanson90@gmail.com>
2021-08-25 19:55:29 +1200

    Update GameObject.java
    -Fixed some java doc
    -Added a new method: name of entity type
    -Added the requirement that game objects must have a movement controller

--- src/nz/ac/vuw/ecs/swen225/gp21/domain/GameObject.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/GameObject.java
@@ -1,4 +1,7 @@
 package nz.ac.vuw.ecs.swen225.gp21.domain;
+
+import nz.ac.vuw.ecs.swen225.gp21.domain.controllers.MovementController;
+
 /**
  * A Game object is something that exists in the game world that can move.
  * @author Benjamin
@@ -12,6 +15,10 @@ abstract class GameObject {
 	 */
 	private final World w;
 	/**
+	 * The logic that GameObjects use to choose how to move is encapsulated away into a movement controller
+	 */
+	private MovementController c;
+	/**
 	 * Create a new GameObject
 	 * @param w
 	 */
@@ -22,7 +29,6 @@ abstract class GameObject {
 	 * GameObjects must state whether an entity can enter onto the same tile as them
 	 * For instance, a bug can enter the same tile as Chip, but a bug cannot enter the same tile as a block
 	 * @param entity the entity trying to move onto the same tile as 'this'
-	 * @param w the game world, in case the entity needs to check some parameters to determine if it can be stepped on
 	 * @return Whether or not the entity can enter the tile 'this' object is on, according to the game logic
 	 */
 	protected abstract boolean canEntityGoOnTile(GameObject entity);
@@ -30,7 +36,6 @@ abstract class GameObject {
 	 * Execute this object's behavior when another GameObject enters the same tile it is on.
 	 * For example, if Chip enters the same tile as a movable block, the block moves itself { w.moveUp(this) }
 	 * @param entity the entity that just entered the tile 'this' object is on
-	 * @param w the game world, in case the object needs to perform some behavior on the world
 	 */
 	protected abstract void entityEnteredTile(GameObject entity);
 	/**
@@ -38,4 +43,11 @@ abstract class GameObject {
 	 * @param elapsedTime time in milliseconds since the last update
 	 */
 	protected abstract void update(double elapsedTime);
+	/**
+	 * Get the name of this object type.
+	 * For example, the player controlled entity will return "Chip"
+	 * 				the bug entity will return "Bug"
+	 * @return the name of this entity
+	 */
+	protected abstract String getName();
 }
