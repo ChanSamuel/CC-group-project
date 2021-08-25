9804e17d1b850ea2f3c93dcf2e9d66b03f1b6364
BudgiePanic <bsanson90@gmail.com>
2021-08-25 17:51:48 +1200

    Added misc. comment

--- src/nz/ac/vuw/ecs/swen225/gp21/domain/World.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/World.java
@@ -1,7 +1,8 @@
 package nz.ac.vuw.ecs.swen225.gp21.domain;
 
 public class World {
-
+	//NOTE: the recording of game ticks can be in a linked list, we dont need to store these in contiguous storage
+	
 	public void moveUp() {
 		// TODO Auto-generated method stub
 		
