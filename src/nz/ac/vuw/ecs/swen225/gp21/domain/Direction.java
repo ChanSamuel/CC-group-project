dfb55454dc7c8b82ef59a17c7f57eff57a4f1cd1
BudgiePanic <bsanson90@gmail.com>
2021-08-25 16:28:55 +1200

    Created direction
    GameObjects have a direction.
    This data can be used by observers.

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/Direction.java
@@ -0,0 +1,27 @@
+package nz.ac.vuw.ecs.swen225.gp21.domain;
+/**
+ * @author Benjamin
+ * GameObjects have a direction
+ */
+public enum Direction {
+	/**
+	 * Object is facing north
+	 */
+	NORTH,
+	/**
+	 * Object is facing south
+	 */
+	SOUTH,
+	/**
+	 * Object is facing east
+	 */
+	EAST,
+	/**
+	 * Object is facing west
+	 */
+	WEST,
+	/**
+	 * Object has no direction
+	 */
+	NONE
+}
