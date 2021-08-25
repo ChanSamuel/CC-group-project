40b7c881a5e34f6898a8a04e920be0410cc252f4
BudgiePanic <bsanson90@gmail.com>
2021-08-25 16:28:24 +1200

    Created GameObject type
    The game object type represents entities in the game that can move, such as Chip, blocks, and bugs

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/GameObject.java
@@ -0,0 +1,9 @@
+package nz.ac.vuw.ecs.swen225.gp21.domain;
+/**
+ * A Game object is something that exists in the game world that can move.
+ * @author Benjamin
+ *
+ */
+abstract class GameObject {
+	
+}
