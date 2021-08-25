2eed40f895a8d0c1cf2e8d685ddcb914cf2dbd99
BudgiePanic <bsanson90@gmail.com>
2021-08-25 20:58:18 +1200

    Added player movement controller to chip
    Chip is now controller by the player movement controller

--- src/nz/ac/vuw/ecs/swen225/gp21/domain/Chip.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/Chip.java
@@ -1,11 +1,17 @@
 package nz.ac.vuw.ecs.swen225.gp21.domain;
-
+/**
+ * Chip is the GameObject that the player will control
+ * He can collect items in his inventory
+ * @author Benjamin
+ *
+ */
 class Chip extends GameObject {
 	/**
-	 * @param w
+	 * Create a new Chip
+	 * @param w the game world that chip exists in.
 	 */
 	protected Chip(World w) {
-		super(w);
+		super(w, new PlayerController(w));
 	}
 	
 	@Override
@@ -24,7 +30,9 @@ class Chip extends GameObject {
 
 	@Override
 	protected void update(double elapsedTime) {
-		c.update(elapsedTime);
+		//I'm not sure if we should be getting the lower level objects to move themselves?
+		//or {currently} send a command back up to the top level class to move the object for us?
+		c.update(elapsedTime).execute(w);
 	}
 
 	@Override
