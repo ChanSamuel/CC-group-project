ac880d79348cc4c2492e9d641c22decc0073b3d6
BudgiePanic <bsanson90@gmail.com>
2021-08-25 16:23:57 +1200

    Created command interface
    The command interface allows external entities to send movement instructions.
    These instructions are then executed by the game.

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/command/Command.java
@@ -0,0 +1,22 @@
+package nz.ac.vuw.ecs.swen225.gp21.domain.command;
+
+import nz.ac.vuw.ecs.swen225.gp21.domain.World;
+
+/**
+ * Command encapsulates an instruction that can be sent from an outside source
+ * to the world, to be executed
+ * @author Benjamin
+ *
+ */
+public interface Command {
+	/**
+	 * Complete this action
+	 * @param w
+	 */
+	void execute(World w);
+	/**
+	 * Undo the action performed by this command
+	 * @param w
+	 */
+	void undo(World w);
+}
