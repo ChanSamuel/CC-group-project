23ac51752ebed4e4fef441d37d294040041add01
BudgiePanic <bsanson90@gmail.com>
2021-08-27 17:56:58 +1200

    Created a tile type
    The exit tile is the final goal for chip.
    If chip enters a tile with this terrain type, the game 'ends' (yet to be determined, world method is invoked)

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/ExitTile.java
@@ -0,0 +1,24 @@
+package nz.ac.vuw.ecs.swen225.gp21.domain;
+/**
+ * The exit tile is the final goal tile
+ * If Chip enters a tile of this type, they win!
+ * @author Benjamin
+ *
+ */
+public class ExitTile extends Terrain {
+
+	@Override
+	public Terrain nextType(GameObject o) { return null; }
+
+	@Override
+	public void entityEntered(GameObject o) {
+		o.w.enteredExit();
+	}
+
+	@Override
+	public boolean canEntityGoOn(GameObject o) {
+		if(o instanceof Chip) return true;
+		return false;
+	}
+
+}
