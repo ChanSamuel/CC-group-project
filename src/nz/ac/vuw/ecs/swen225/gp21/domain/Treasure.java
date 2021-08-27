db69f424bcfb84c26dedcd07631619247c041f80
BudgiePanic <bsanson90@gmail.com>
2021-08-27 18:08:12 +1200

    Created tile type
    The treasure type represents the tiles that Chip must enter to collect treasure. When all the treasure is collected, the exit lock tile becomes a normal tile, allowing chip to exit the level

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/Treasure.java
@@ -0,0 +1,29 @@
+package nz.ac.vuw.ecs.swen225.gp21.domain;
+/**
+ * The treasure tile terrain type represents a tile with treasure on it
+ * chip must collect all the treasure to unlock the exit lock (step on all tiles with this terrain type)
+ * Only Chip can enter this type of tile
+ * @author Benjamin
+ *
+ */
+public class Treasure extends Terrain {
+
+	@Override
+	public Terrain nextType(GameObject o) {
+		return new Free();
+	}
+
+	@Override
+	public boolean canEntityGoOn(GameObject o) {
+		//only chip is allowed on these tiles
+		if(o instanceof Chip) return true;
+		return false;
+	}
+	
+	@Override
+	public void entityEntered(GameObject o) {
+		Chip player = (Chip) o;
+		player.collectedChip();
+	}
+
+}
