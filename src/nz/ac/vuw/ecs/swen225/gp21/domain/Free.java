bc9701a10a4fe2e7d5bfca80dbe263712de0ba8b
BudgiePanic <bsanson90@gmail.com>
2021-08-27 17:57:31 +1200

    Created tile type
    The free terrain type is the simplest, anyone can move onto it, and nothing happens if they do

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/Free.java
@@ -0,0 +1,19 @@
+package nz.ac.vuw.ecs.swen225.gp21.domain;
+/**
+ * The free terrain type is the simplest. 
+ * Any GameObject can enter a free terrain type.
+ * @author Benjamin
+ *
+ */
+public class Free extends Terrain {
+
+	@Override
+	public Terrain nextType(GameObject o) { return this; }
+
+	@Override
+	public void entityEntered(GameObject o) {} //simple behavior for free tiles 
+
+	@Override
+	public boolean canEntityGoOn(GameObject o) { return true; } //any object can go on a free tile type
+
+}
