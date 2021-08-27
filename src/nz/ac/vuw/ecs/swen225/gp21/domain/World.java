e10df90028424151b268d69a3057a5b9eb2c8d3b
BudgiePanic <bsanson90@gmail.com>
2021-08-27 20:20:29 +1200

    added toString && update method now updates GameObjects
    Also added a useful toString method for debugging

--- src/nz/ac/vuw/ecs/swen225/gp21/domain/World.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/World.java
@@ -18,6 +18,8 @@ public class World {
 	private List<GameObject> allEntities;
 	
 	private Board board;
+	
+	private boolean isGameOver;
 //===========================================================
 	/**
 	 * Create a new test world using a test board
@@ -28,7 +30,9 @@ public class World {
 		board = new ArrayBoard();
 		playerEntity = new Chip(this);
 		allEntities = new ArrayList<GameObject>();
+		allEntities.add(playerEntity); //always update the player entity first
 		board.addObject(playerEntity, new Coord(0,0));
+		isGameOver = false;
 		//board.validate() ?
 	}
 //===========================================================
@@ -42,11 +46,21 @@ public class World {
 		//Process one player input { ? should we defer this to the 'player movement controller', or do it here, explicitly ? }
 		
 		//update all game objects
-		
+		for(GameObject e : allEntities) e.update(elapsedTime); 
 		//check game state (i.e. is the game over?)
-		
+		if(isGameOver) System.out.println("Game is over"); //do something?
 		//Encapsulate all the events that occurred in this game tick and store it, so other modules can view what happened during this tick
+		//TODO
 	}
+	/**
+	 * Return the isGameOver flag
+	 * Will be replaced in the future by the state variable
+	 * @return true if the game is in a complete state
+	 */
+	public boolean isGameComplete() {
+		return this.isGameOver;
+	}
+	
 //==========================================================
 	/**
 	 * Remove the top element player commands queue
@@ -118,14 +132,36 @@ public class World {
 	/**
 	 * This method is called when chip collects a treasure
 	 */
-	public void collectedAChip() {
+	void collectedAChip() {
 		playerEntity.treasureCollected++;
 		if(board.getRemainingChips() == 0) board.openExit();
 	}
 	/**
 	 * This method is called when chip enters the exit square
 	 */
-	public void enteredExit() {
+	void enteredExit() {
+		isGameOver = true;
 //		worldState = new GameOver(); //TODO
 	}
+//====================================================================================
+	@Override
+	public String toString() {
+		StringBuilder ans = new StringBuilder();
+		//add board state
+		ans.append("Is game over? -> "+isGameOver+"\n");
+		//add player inventory
+		//add queue
+		ans.append("PlayerQueue: \n");
+		if(playerCommands.isEmpty()) ans.append("EMPTY");
+		for(Command c : playerCommands) ans.append(c+"\n");
+		ans.append("\n");
+		//add all entities
+		ans.append("All entities: \n");
+		for(GameObject e : allEntities) ans.append(e+"\n");
+		ans.append("\n");
+		//add board
+		ans.append("Board: \n"+board);
+		return ans.toString();
+	}
+	
 }
