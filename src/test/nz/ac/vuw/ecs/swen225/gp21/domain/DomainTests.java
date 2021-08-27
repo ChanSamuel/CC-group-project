14741d8e5797daf34fd4eef2401095477142056a
BudgiePanic <bsanson90@gmail.com>
2021-08-27 13:54:09 +1200

    Created test file
    Unit tests for the domain package will be put here

---
+++ src/test/nz/ac/vuw/ecs/swen225/gp21/domain/DomainTests.java
@@ -0,0 +1,18 @@
+package test.nz.ac.vuw.ecs.swen225.gp21.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+/**
+ * These tests test the functionality of the Domain package
+ * @author Benjamin
+ *
+ */
+class DomainTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
