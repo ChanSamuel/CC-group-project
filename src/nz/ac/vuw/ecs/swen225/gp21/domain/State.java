1da1ec4ec3c5063e14981ea27a5e1ebb74ba69a4
BudgiePanic <bsanson90@gmail.com>
2021-08-28 19:35:48 +1200

    Defined the state interface
    Relates #5
    The world object has a state that determines when it can perform certain actions.

--- src/nz/ac/vuw/ecs/swen225/gp21/domain/State.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/State.java
@@ -1,4 +1,5 @@
 package nz.ac.vuw.ecs.swen225.gp21.domain;
+
 /**
  * The Game world can be in one of several states, this interface defines the operations a state 
  * most perform.
@@ -6,6 +7,53 @@ package nz.ac.vuw.ecs.swen225.gp21.domain;
  *
  */
 public interface State {
-	public void update(World w);
-	//TODO add more stuff here
+	/**
+	 * Simulate the world for one tick
+	 * @param w
+	 */
+	public void update(World w, double elapsedTime);
+	/**
+	 * Get the width of board
+	 * @param w 
+	 * @return the height of the world
+	 */
+	public int getBoardWidth(World w);
+	/**
+	 * Get the height of the board
+	 * @param w
+	 * @return the height of the board
+	 */
+	public int getBoardHeight(World w);
+	/**
+	 * Determine if a Coordinate is valid
+	 * @param w
+	 * @return true if the coordinate is valid in the world
+	 */
+	public boolean isCoordValid(World w, Coord c);
+	/**
+	 * Add a new Object to the world
+	 * @param w
+	 * @return if the object was added successfully
+	 */
+	public boolean addObject(World w, GameObject e, Coord c);
+	/**
+	 * Enqueue a move left command for chip
+	 * @param w
+	 */
+	public void moveChipLeft(World w);
+	/**
+	 * Enqueue a move up command for chip
+	 * @param w
+	 */
+	public void moveChipUp(World w);
+	/**
+	 * Enqueue a move down command for chip
+	 * @param w
+	 */
+	public void moveChipDown(World w);
+	/**
+	 * Enqueue a move right command for chip
+	 * @param w
+	 */
+	public void moveChipRight(World w);
 }
