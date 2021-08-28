b12cb27082f1b5b54d358b8875ce73e92a20a33e
BudgiePanic <bsanson90@gmail.com>
2021-08-28 15:39:40 +1200

    Changed how teleporter tile works
    The previous way the teleporter worked caused issues when it came to edge cases.
    This new way makes teleporting easy, as long as the level designer understands how the teleporters work. Updated javaDoc should help with their understanding.

--- src/nz/ac/vuw/ecs/swen225/gp21/domain/Teleporter.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/Teleporter.java
@@ -3,14 +3,27 @@ package nz.ac.vuw.ecs.swen225.gp21.domain;
 import java.util.HashMap;
 import java.util.Map;
 /**
- * The teleporter is a special tile that will move an entity to a linked tile 
+ * The teleporter is a special tile that will move an entity to the tile one after the link tile in the direction the GameObject was moving. 
  * when it is entered. The board loader must ensure the teleporter links (map entries) have been created
+ * Example teleport behavior:
+ * Before:
+ * |_|_|_|
+ * |_|O|_|
+ * |#|#|#|
+ * |_|O|_|
+ * |^|C|_|
+ * After:
+ * |_|C|_|
+ * |_|O|_|
+ * |#|#|#|
+ * |_|O|_|
+ * |_|_|_|
  * @author Benjamin
  *
  */
 public class Teleporter extends Terrain {
 	/**
-	 * Record one-way teleporter links
+	 * Record of one-way teleporter links
 	 */
 	public static Map<Coord, Coord> links = new HashMap<>();
 	
@@ -19,17 +32,41 @@ public class Teleporter extends Terrain {
 
 	@Override
 	public void entityEntered(GameObject o) {
-		//if the teleport tile has not link the level was not initialized properly
-		Coord destination = links.get(o.currentTile.location);
-		if(destination == null) throw new IllegalStateException("Teleporter at "+o.currentTile.location+" has no link!");
-		o.w.moveObject(o, destination);	
+		Coord destination = getDestinationCoord(o);
+		o.w.moveObject(o, destination);
 	}
 
 	@Override
-	public boolean canEntityGoOn(GameObject o) { return true; }//any entity can go through the teleporter
+	public boolean canEntityGoOn(GameObject o) { 
+		Coord destination = getDestinationCoord(o);
+		if(!o.w.isCoordValid(destination)) return false;
+		return o.w.getTileAt(destination).canEntityGoOnTile(o);
+	}
 
 	@Override
 	public char boardChar() { return 'O'; }
+	/**
+	 * Gets the coordinate of the tile the teleporter will try to move the GameObject to
+	 * Based on the direction the GameObject is facing
+	 * @param o The Object moving onto the teleporter
+	 * @return the coordinate of the tile the teleporter will move the object to
+	 */
+	private Coord getDestinationCoord(GameObject o) {
+		Coord destination = links.get(o.currentTile.location); //if the teleport tile has no link the level was not initialized properly
+		if(destination == null) throw new IllegalStateException("Teleporter at "+o.currentTile.location+" has no link!");
+		switch(o.dir) {
+		case EAST:
+			return destination.right();
+		case NORTH:
+			return destination.up();
+		case SOUTH:
+			return destination.down();
+		case WEST:
+			return destination.left();
+		default: 
+			throw new RuntimeException("Objects moving into teleporter must have a direction to signal where they are going! ->"+o.toString());
+		}
+	}
 	
 
 }
