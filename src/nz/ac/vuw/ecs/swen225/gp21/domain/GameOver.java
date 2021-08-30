eb84334328483f477b80eed2f47731b1fa581a21
BudgiePanic <bsanson90@gmail.com>
2021-08-30 15:50:19 +1200

    Added GameOver state
    The game over state is used to signal that the game simulation is complete
    (either Chip won, or lost)

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/GameOver.java
@@ -0,0 +1,55 @@
+package nz.ac.vuw.ecs.swen225.gp21.domain;
+/**
+ * The running state represents a world that has completed its gameplay simulation 
+ * and is in a finished state.
+ * @author Benjamin
+ *
+ */
+public final class GameOver implements State {
+
+	@Override
+	public void update(World w, double elapsedTime) {
+		throw new IllegalStateException("World is in game over state! No more updates can be made!");
+	}
+
+	@Override
+	public int getBoardWidth(World w) {
+		return w.getBoard().getWidth();
+	}
+
+	@Override
+	public int getBoardHeight(World w) {
+		return w.getBoard().getHeight();
+	}
+
+	@Override
+	public boolean isCoordValid(World w, Coord c) {
+		return w.isCoordValid(c);
+	}
+
+	@Override
+	public boolean addObject(World w, GameObject e, Coord c) {
+		throw new IllegalStateException("World is in game over state! No more objects can be added");
+	}
+
+	@Override
+	public void moveChipLeft(World w) {
+		throw new IllegalStateException("World is in game over state! Cannot enqueue any more moves");
+	}
+
+	@Override
+	public void moveChipUp(World w) {
+		throw new IllegalStateException("World is in game over state! Cannot enqueue any more moves");
+	}
+
+	@Override
+	public void moveChipDown(World w) {
+		throw new IllegalStateException("World is in game over state! Cannot enqueue any more moves");
+	}
+
+	@Override
+	public void moveChipRight(World w) {
+		throw new IllegalStateException("World is in game over state! Cannot enqueue any more moves");
+	}
+
+}
