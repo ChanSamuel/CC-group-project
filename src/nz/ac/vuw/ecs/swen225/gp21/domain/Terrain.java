bf68a7eb4e498e4c7c882c36e66457e07eec805a
BudgiePanic <bsanson90@gmail.com>
2021-08-30 13:53:15 +1200

    Updated some javaDoc
    It is useful to know these methods are called in a certain order by the terrain class

--- src/nz/ac/vuw/ecs/swen225/gp21/domain/Terrain.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/Terrain.java
@@ -9,12 +9,14 @@ package nz.ac.vuw.ecs.swen225.gp21.domain;
 public abstract class Terrain {
 	/**
 	 * When an object enters a tile with this terrain, the terrain may swap to another
+	 * This method is invoked after entityEntered() is invoked.
 	 * @param o the GameObject that entered a tile with this terrain type
 	 * @return the terrain type the tile should now have
 	 */
 	public abstract Terrain nextType(GameObject o);
 	/**
 	 * Conduct activities when an object enters a tile of this type 
+	 * This method is invoked before setting the Tile's terrain to this.nextType()
 	 * @param o the object that entered a tile with this type
 	 */
 	public abstract void entityEntered(GameObject o);
