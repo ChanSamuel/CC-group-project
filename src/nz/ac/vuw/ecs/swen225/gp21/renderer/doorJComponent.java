1da7b730c8d3f9db8f39e9991b4af42e5cec9cb1
Meng <limeng7@ecs.vuw.ac.nz>
2021-08-31 13:42:27 +1200

    create a JComponent for draw door

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/renderer/doorJComponent.java
@@ -0,0 +1,29 @@
+package nz.ac.vuw.ecs.swen225.gp21.renderer;
+
+import nz.ac.vuw.ecs.swen225.gp21.domain.Coord;
+
+/**
+ * This is the door Jcomponent, update when door unlocked.
+ * @author mengli
+ *
+ */
+public class doorJComponent extends GameObjectJComponent{
+
+	doorJComponent(Coord coord, int width, int height, String filename) {
+		super(coord, width, height, filename);
+		
+	}
+	/**
+	 * start the door animation when door opened.
+	 */
+	void openDoor() {
+		this.repaint();
+		//TODO
+		DoorMoving dm = new DoorMoving();
+	}
+
+}
+//TODO
+class DoorMoving extends Thread{
+	
+}
