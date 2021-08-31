e347d645a0fc8014dbfc79f88619bb19907520fa
Meng <limeng7@ecs.vuw.ac.nz>
2021-08-31 13:41:53 +1200

    Create abstract class for draw all the Game Objects

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/renderer/GameObjectJComponent.java
@@ -0,0 +1,30 @@
+package nz.ac.vuw.ecs.swen225.gp21.renderer;
+
+import java.awt.Graphics;
+
+import javax.swing.JComponent;
+
+import nz.ac.vuw.ecs.swen225.gp21.domain.Coord;
+
+/**
+ * This is an abstract class for all the game object
+ * @author mengli
+ *
+ */
+public abstract class GameObjectJComponent extends JComponent{
+	/**
+	 * Constructor
+	 * Take coordinates, size and filename as parameters
+	 */
+	GameObjectJComponent(Coord coord,int width, int height,String filename) {
+		super();
+		//TODO
+	}
+	/**
+	 * override paintComponent method,draw chap
+	 */
+	@Override
+	public void paintComponent(Graphics g) {
+		//TODO 
+	}
+}
