abbda8e8459804b7328c3c4ca5c65b2216940a19
Meng Li <limeng7@ecs.vuw.ac.nz>
2021-09-02 12:57:45 +1200

    create background JPanel

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/renderer/BackgroundJPanel.java
@@ -0,0 +1,28 @@
+package nz.ac.vuw.ecs.swen225.gp21.renderer;
+
+import java.awt.Graphics;
+import java.util.List;
+
+import javax.swing.JPanel;
+
+/**
+ * This is the backgound JPanel which draws the tiles and walls
+ * @author mengli
+ *
+ */
+public class BackgroundJPanel extends JPanel{
+	/**
+	 * The constructor
+	 */
+	BackgroundJPanel(/*List<Tile> board*/){
+		
+	}
+	/**
+	 * Override the paint method, so it can draw the 
+	 */
+	@Override
+	public void paint(Graphics g) {
+		
+	}
+
+}
