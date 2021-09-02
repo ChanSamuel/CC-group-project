31024fd6a2cf2e7aaee7b29bf7f346190c5290db
Peter Liley <43123543+petlil@users.noreply.github.com>
2021-09-02 17:03:20 +1200

    Updated to match 'list of ticks' model

--- src/nz/ac/vuw/ecs/swen225/gp21/recorder/Recorder.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/recorder/Recorder.java
@@ -1,5 +1,6 @@
 package nz.ac.vuw.ecs.swen225.gp21.recorder;
 
+import java.io.File;
 import java.util.List;
 
 /**
@@ -10,13 +11,13 @@ import java.util.List;
  * @author Peter Liley 2021
  */
 public class Recorder {
-    List<GameTick> gameStates;
-    int statePointer;
+    List<GameTick> ticks;
+    int tickPointer;
     private boolean autoReplayRunning = false;
     private float autoReplaySpeed;
 
     public Recorder(){
-        this.statePointer = 0;
+        this.tickPointer = 0;
         this.autoReplaySpeed = 1.0f;
     }
 
@@ -25,8 +26,8 @@ public class Recorder {
      * 
      * return: True if save successful
      */
-    public boolean save(List<GameTick> statesToSave){
-
+    public boolean save(){
+        return SaveRecording.save(ticks);
     }
 
     /**
@@ -34,35 +35,53 @@ public class Recorder {
      * @return A list of all game states in loaded recording
      */
     public List<GameTick> load(File xmlToLoad){
-
+        return LoadRecording.load(xmlToLoad);
     }
 
     /**
-     * Returns the next game state
-     * TODO: this could involve an algorithm that sifts through game states to find 'interesting'
-     *       ones where things actually change
+     * Add a single tick to the list
+     * @throws IllegalArgumenException if tick is not valid (e.g. null ticks)
+     */
+    public void addTick(GameTick tick){
+        if(tickValid(tick)) ticks.add(tick);
+        else throw new IllegalArgumentException();
+    }
+
+    /**
+     * Returns true if the tick is valid
+     */
+    private boolean tickValid(GameTick tick) {
+        if(tick == null) return false;
+        return true;
+    }
+
+    /**
+     * Returns the next game state in which an actor moves
      * @return The next meaningful game state
      */
-    public GameState playOneStep(){
-
+    public GameTick playOneStep(){
+        while(!ticks.get(tickPointer).tickIsMeaningful()){
+            tickPointer++;
+        }
+        return ticks.get(tickPointer);
     }
 
     /**
-     * Updates state pointer and returns game state one ahead in the list
+     * Returns the next tick in the list of ticks by incrementing the pointer.
      * @return Next game state index in the list (or current state if no prev state)
      */
-    private GameState next() {
-        if(statePointer < gameStates.length) statePointer++;
-        return gameStates[statePointer];
+    private GameTick next() {
+        if(tickPointer < ticks.size()) tickPointer++;
+        return ticks.get(tickPointer);
     }
 
     /**
      * Updates state pointer and returns game state one before in the list
      * @return Previous game state index in the list (or current state if no prev state)
      */
-    private GameState prev() {
-        if(statePointer > 0) statePointer--;
-        return gameStates[statePointer];
+    private GameTick prev() {
+        if(tickPointer > 0) tickPointer--;
+        return ticks.get(tickPointer);
     }
 
     /**
@@ -70,7 +89,7 @@ public class Recorder {
      */
     public void setAutoReplayRunning(boolean running){
          autoReplayRunning = running;
-    }
+    } 
 
     /**
      * Sets auto replay speed 
--- src/nz/ac/vuw/ecs/swen225/gp21/recorder/SaveRecording.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/recorder/SaveRecording.java
@@ -1,5 +1,7 @@
 package nz.ac.vuw.ecs.swen225.gp21.recorder;
 
+import java.util.List;
+
 /**
  * This class saves a list of game states to an xml file.
  * Uses the Persistency package's xml encoder to encode individual game states
@@ -7,5 +9,9 @@ package nz.ac.vuw.ecs.swen225.gp21.recorder;
  * 
  */
 public class SaveRecording {
+
+    public static boolean save(List<GameTick> statesToSave) {
+        return false;
+    }
     
 }
