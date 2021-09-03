71b74aecadf2f5acae9f85c26572c9803ff3759b
Meng Li <limeng7@ecs.vuw.ac.nz>
2021-09-03 22:46:10 +1200

    create a temporary main function to try GUI

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/renderer/tempMain.java
@@ -0,0 +1,15 @@
+package nz.ac.vuw.ecs.swen225.gp21.renderer;
+
+import nz.ac.vuw.ecs.swen225.gp21.domain.World;
+
+/**
+ * A temp main function for play with maze.
+ * @author mengli
+ *
+ */
+public class tempMain {
+	public static void main(String arg[]) {
+		World world = new World();
+		WorldJFrame worldJFrame = new WorldJFrame(world);
+	}
+}
