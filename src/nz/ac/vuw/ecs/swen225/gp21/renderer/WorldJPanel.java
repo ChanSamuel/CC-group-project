9dc806ca334036b92af7dfa5cb43766b5bda9f3e
Meng Li <limeng7@ecs.vuw.ac.nz>
2021-09-05 09:02:46 +1200

    change chap JComponent to JPanel

--- src/nz/ac/vuw/ecs/swen225/gp21/renderer/WorldJPanel.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/renderer/WorldJPanel.java
@@ -1,6 +1,10 @@
 package nz.ac.vuw.ecs.swen225.gp21.renderer;
 
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
+import javax.swing.RepaintManager;
+
+import nz.ac.vuw.ecs.swen225.gp21.domain.ArrayBoard;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Board;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Chip;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Coord;
@@ -34,9 +38,9 @@ public class WorldJPanel extends JPanel{
 	 */
 	private Chip chap;
 	/**
-	 * The chap Jcomponent
+	 * The chap JPanel
 	 */
-	private ChapJComponent chapJComponent;
+	private ChapJPanel ChapJPanel;
 	/**
 	 * Constructor
 	 */
@@ -50,16 +54,29 @@ public class WorldJPanel extends JPanel{
 		setBounds(0,0,WorldJFrame.WIDTH,WorldJFrame.HEIGHT);
 		System.out.println(this.board.getWidth());
 		setVisible(true);
+		//start creating elements on this JPanel
+		//The background JPanel
+		BackgroundJPanel backgroundJPanel = new BackgroundJPanel(board);
+		//The chap JPanel
 		chap = w.getPlayer();
-		chapJComponent = new ChapJComponent(chap.getCurrentTile().getCoord(),chap.getDir());
-		add(chapJComponent);
+		ChapJPanel = new ChapJPanel(chap.getCurrentTile().getCoord(),chap.getDir());
+		//create a layered pane
+		JLayeredPane lp = new JLayeredPane();
+		int index = 1;
+		lp.setLayout(null);
+		//arrange the layer, smaller index on top.
+		lp.add(ChapJPanel,index++);
+		lp.add(backgroundJPanel,1000);
+		lp.setVisible(true);
+		lp.setBounds(0, 0, WorldJFrame.WIDTH,WorldJFrame.HEIGHT);
+		add(lp);
 	}
 	/**
 	 * update the panel, once chap moves
 	 */
 	public void updateJPanel(Coord coord,Direction dir) {
 		//pass the new coord and direction here to update chap's location
-		chapJComponent.updateChap(coord,dir);
+		ChapJPanel.updateChap(coord,dir);
 		//calculate the offset of chap's coord from center of the board.
 		int diffX = TILE_WIDTH*((WorldJFrame.FOCUS_AREA_COLS-1)/2-coord.getCol());
 		int diffY = TILE_HEIGHT*((WorldJFrame.FOCUS_AREA_ROWS-1)/2-coord.getRow());
