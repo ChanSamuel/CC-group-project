825d7e08b3086e75b348e809870436d1a967be8b
Meng Li <limeng7@ecs.vuw.ac.nz>
2021-09-05 13:58:19 +1200

    add some comments and getBoard() and getChap methods

--- src/nz/ac/vuw/ecs/swen225/gp21/renderer/WorldJPanel.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/renderer/WorldJPanel.java
@@ -2,9 +2,7 @@ package nz.ac.vuw.ecs.swen225.gp21.renderer;
 
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
-import javax.swing.RepaintManager;
 
-import nz.ac.vuw.ecs.swen225.gp21.domain.ArrayBoard;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Board;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Chip;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Coord;
@@ -42,46 +40,68 @@ public class WorldJPanel extends JPanel{
 	 */
 	private ChapJPanel ChapJPanel;
 	/**
+	 * The changingTerrainJPanel, this is the JPanel holding those changing terrain types.
+	 */
+	private ChangingTerrainJPanel changingTerrainJPanel;
+	/**
 	 * Constructor
 	 */
 	public WorldJPanel(World w) {
-		//TODO
-		this.w = w;
+		//-------- Set the world and board ----------------------------------------------
+				this.w = w;
+				this.board = w.getBoard();
+		//-------- Set the properties of this JPanel -----------------------------------
 		setLayout(null);
-		setVisible(false);
-		this.board = w.getBoard();
-		//by changing the bounds of current JPanel, moving the focus area.
-		setBounds(0,0,WorldJFrame.WIDTH,WorldJFrame.HEIGHT);
-		System.out.println(this.board.getWidth());
 		setVisible(true);
-		//start creating elements on this JPanel
+		setBounds(0,0,WorldJFrame.WIDTH,WorldJFrame.HEIGHT);
+		//-------- Start creating elements on this JPanel--------------------------------
 		//The background JPanel
-		BackgroundJPanel backgroundJPanel = new BackgroundJPanel(board);
+		BackgroundJPanel backgroundJPanel = new BackgroundJPanel(this);
+		//The changingTerrain JPanel
+		this.changingTerrainJPanel = new ChangingTerrainJPanel(this);
 		//The chap JPanel
 		chap = w.getPlayer();
-		ChapJPanel = new ChapJPanel(chap.getCurrentTile().getCoord(),chap.getDir());
-		//create a layered pane
+		this.ChapJPanel = new ChapJPanel(this);
+		//--------- Create a layered pane and add elements to this pane ------------------
 		JLayeredPane lp = new JLayeredPane();
 		int index = 1;
 		lp.setLayout(null);
 		//arrange the layer, smaller index on top.
 		lp.add(ChapJPanel,index++);
+		lp.add(changingTerrainJPanel,index++);
 		lp.add(backgroundJPanel,1000);
 		lp.setVisible(true);
 		lp.setBounds(0, 0, WorldJFrame.WIDTH,WorldJFrame.HEIGHT);
+		//add this JPanel to worldJPanel.
 		add(lp);
 	}
 	/**
 	 * update the panel, once chap moves
 	 */
-	public void updateJPanel(Coord coord,Direction dir) {
+	void updateJPanel() {
+		//-------------Update all the changed JPanels---------------------
 		//pass the new coord and direction here to update chap's location
-		ChapJPanel.updateChap(coord,dir);
+		this.ChapJPanel.updateChap();
+		//repaint the changingTerrainJPanel.
+		this.changingTerrainJPanel.repaint();
+		
 		//calculate the offset of chap's coord from center of the board.
-		int diffX = TILE_WIDTH*((WorldJFrame.FOCUS_AREA_COLS-1)/2-coord.getCol());
-		int diffY = TILE_HEIGHT*((WorldJFrame.FOCUS_AREA_ROWS-1)/2-coord.getRow());
+		int diffX = TILE_WIDTH*((WorldJFrame.FOCUS_AREA_COLS-1)/2-chap.getCurrentTile().getCoord().getCol());
+		int diffY = TILE_HEIGHT*((WorldJFrame.FOCUS_AREA_ROWS-1)/2-chap.getCurrentTile().getCoord().getRow());
 		//change the location of panel to place chap in the center.
 		setBounds(diffX,diffY,WorldJFrame.WIDTH,WorldJFrame.HEIGHT);
 	}
+	/**
+	 * Get the board
+	 */
+	Board getBoard() {
+		return this.board;
+	}
+	/**
+	 * Get the chap
+	 */
+	Chip getChap() {
+		return this.chap;
+	} 
 	
 }
