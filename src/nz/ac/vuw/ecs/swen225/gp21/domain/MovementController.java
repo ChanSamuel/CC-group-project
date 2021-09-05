ec23116ac128c3209c1a8b803369103f03e7cfff
BudgiePanic <bsanson90@gmail.com>
2021-09-05 16:50:31 +1200

    Updated some java doc on movement controllers

--- src/nz/ac/vuw/ecs/swen225/gp21/domain/MovementController.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/MovementController.java
@@ -14,6 +14,8 @@ package nz.ac.vuw.ecs.swen225.gp21.domain;
 public interface MovementController {
 	/**
 	 * Generate the command that the object this controller represents wants to do.
+	 * NOTE: If your movement controller decides to do nothing, it should append a new NoMove()
+	 * to world.event to help produce a record of all the actions that occured.
 	 * @param w the world this move will be applied to
 	 * @param elapsedTime the time since the last game update - this may be changed in favour for a different game loop that uses fixed time steps
 	 * @return the command the gameObject wants to perform
