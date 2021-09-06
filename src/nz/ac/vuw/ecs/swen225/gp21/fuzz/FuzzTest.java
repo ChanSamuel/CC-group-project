534752aa77cbbcdcf1e2787038a8acc87a5085d0
Marcus Buckland <alreadysignedin@gmail.com>
2021-09-06 12:37:06 +1200

    Random key-press method created.

--- src/nz/ac/vuw/ecs/swen225/gp21/fuzz/FuzzTest.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/fuzz/FuzzTest.java
@@ -1,4 +1,35 @@
 package nz.ac.vuw.ecs.swen225.gp21.fuzz;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
 public class FuzzTest {
+    private final Robot robot = new Robot();
+    private final Random random = new Random();
+
+    public FuzzTest() throws AWTException {
+    }
+
+    public void generateKeyPress() throws AWTException {
+
+        switch (random.nextInt(4)) {
+            case 0 -> robot.keyPress(KeyEvent.VK_LEFT);
+            case 1 -> robot.keyPress(KeyEvent.VK_RIGHT);
+            case 2 -> robot.keyPress(KeyEvent.VK_UP);
+            case 3 -> robot.keyPress(KeyEvent.VK_DOWN);
+        }
+
+    }
+
+    public static void main(String[] args) throws AWTException {
+        FuzzTest fuzzTest = new FuzzTest();
+        for (int i=0; i<100; ++i) {
+            fuzzTest.generateKeyPress();
+        }
+
+
+    }
+
+
 }
