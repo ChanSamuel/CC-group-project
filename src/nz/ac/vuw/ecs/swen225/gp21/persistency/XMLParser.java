b55e3a6a34dde4299ac5e7f3f9339984eb23103a
lucygoodie <79559998+lucygoodie@users.noreply.github.com>
2021-09-14 12:58:38 +1200

    Squashed commit of the following:
    commit c4fac134918f76b4e05fb46ffb45ef2c7a25090d
    Author: lucygoodie <79559998+lucygoodie@users.noreply.github.com>
    Date:   Tue Sep 14 12:54:41 2021 +1200
    
        Update .gitignore
    
    commit 9de15758bd8839cb2cc7454c668d4244f7ef4ebd
    Author: lucygoodie <79559998+lucygoodie@users.noreply.github.com>
    Date:   Tue Sep 14 12:52:09 2021 +1200
    
        Update ConcretePersister.java
    
        updated to matcb intereface
    
    commit 3fef9b6dfd514cd41ad16a73e1cc6f41a0b400a2
    Author: lucygoodie <79559998+lucygoodie@users.noreply.github.com>
    Date:   Tue Sep 14 12:51:59 2021 +1200
    
        Update Persister.java
    
        Updated interface to be in line with group expectations around how modules will interact
    
    commit 9199b635ad38c1d0475f260b252455ec2a78c600
    Author: lucygoodie <79559998+lucygoodie@users.noreply.github.com>
    Date:   Tue Sep 14 12:51:06 2021 +1200
    
        Delete CustomClassLoader.java
    
    commit 449bf68914692a1363e721d4465bc9e66d9b087d
    Author: lucygoodie <79559998+lucygoodie@users.noreply.github.com>
    Date:   Tue Sep 14 12:11:10 2021 +1200
    
        initial class loader class
    
        subject to change
    
    commit 247d3a99d5af7b9221cbbf0f39921f1401dc2b71
    Author: lucygoodie <79559998+lucygoodie@users.noreply.github.com>
    Date:   Mon Sep 13 15:11:33 2021 +1200
    
        Update CustomClassLoader.java
    
        added method signatures for exceptions
    
    commit a44c1c3eda2dffc8999d4d563d91ce49f73c4211
    Author: lucygoodie <79559998+lucygoodie@users.noreply.github.com>
    Date:   Mon Sep 13 15:11:05 2021 +1200
    
        Create CustomClassLoader.java
    
        Custom class loader, not really sure what I am meant to do here
    
    commit 051e0baa5e9970b13539987c0977fe983454790b
    Author: lucygoodie <79559998+lucygoodie@users.noreply.github.com>
    Date:   Mon Sep 13 15:09:36 2021 +1200
    
        Update ConcretePersister.java
    
        updated level  file path
    
    commit d2114df7a391ade583a4b422a9e6d5bce0fe437f
    Author: lucygoodie <79559998+lucygoodie@users.noreply.github.com>
    Date:   Mon Sep 13 09:13:10 2021 +1200
    
        Update ConcretePersister.java
    
        implemented a save level method and updated some of the other methods
    
    commit 23b5529133f58148f1cbbd4097a77795604c9c1e
    Author: lucygoodie <79559998+lucygoodie@users.noreply.github.com>
    Date:   Mon Sep 13 09:12:48 2021 +1200
    
        Update XMLParser.java
    
    commit 19ca8051a63e37d0df257941fce05a0fb02806a2
    Author: lucygoodie <79559998+lucygoodie@users.noreply.github.com>
    Date:   Mon Sep 13 09:12:38 2021 +1200
    
        Update XMLParserTests.java
    
        update to the parser tests
    
    commit 803f3f6ddb72868ac8bbc9aa170192596cc0e731
    Author: lucygoodie <79559998+lucygoodie@users.noreply.github.com>
    Date:   Mon Sep 13 09:12:17 2021 +1200
    
        Update Persister.java
    
        updated some of the methods in the interface
    
    commit fba8b054346b8d029b1214ea224063fa2c7c7f6d
    Author: lucygoodie <79559998+lucygoodie@users.noreply.github.com>
    Date:   Mon Sep 13 08:30:18 2021 +1200
    
        Updates to Persistency tests
    
    commit 1d1ec9a2c173c7caff550ff5c48c6cd410b0be7e
    Author: lucygoodie <79559998+lucygoodie@users.noreply.github.com>
    Date:   Mon Sep 13 08:14:16 2021 +1200
    
        Persistency commit

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/persistency/XMLParser.java
@@ -0,0 +1,64 @@
+package nz.ac.vuw.ecs.swen225.gp21.persistency;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import nz.ac.vuw.ecs.swen225.gp21.domain.*;
+
+public class XMLParser<T> {
+
+    private XmlMapper xmlMapper;
+    private Class<T> valueType;
+
+    public XMLParser(XmlMapper xmlMapper, Class<T> valueType) {
+        this.xmlMapper = xmlMapper;
+        this.valueType = valueType;
+    }
+
+    /**
+     * Method for loading from XML
+     * @param is input stream
+     * @return
+     * @throws PersistException
+     */
+    public T load(InputStream is) throws PersistException {
+        try {
+            return xmlMapper.readValue(is, valueType);
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+            throw new PersistException(""); // todo handle this
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+            throw new PersistException(""); // todo handle this
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new PersistException(""); // todo handle this
+        }
+    }
+
+    /**
+     * Method for saving to XML
+     * @param file
+     * @param value
+     * @throws PersistException
+     */
+    public void save(File file, T value) throws PersistException {
+        try {
+            xmlMapper.writeValue(file, value);
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+            throw new PersistException(""); // todo handle this
+        } catch (JsonGenerationException e) {
+            e.printStackTrace();
+            throw new PersistException(""); // todo handle this
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new PersistException(""); // todo handle this
+        }
+    }
+}
