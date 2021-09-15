2f2f062d318aaadc90c48d720c201867bfcc6083
lucygoodie <79559998+lucygoodie@users.noreply.github.com>
2021-09-15 17:32:11 +1200

    Create LevelHandlingTests.java
    Further testing

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/persistency/tests/LevelHandlingTests.java
@@ -0,0 +1,68 @@
+package nz.ac.vuw.ecs.swen225.gp21.persistency.tests;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Level;
+import nz.ac.vuw.ecs.swen225.gp21.persistency.ConcretePersister;
+import nz.ac.vuw.ecs.swen225.gp21.persistency.LevelHandler;
+import nz.ac.vuw.ecs.swen225.gp21.persistency.PersistException;
+import nz.ac.vuw.ecs.swen225.gp21.persistency.XMLParser;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class LevelHandlingTests {
+
+    @Test
+    public void saveLevel1() throws PersistException {
+        String tiles = "";
+        tiles += "################";
+        tiles += "#..........#...#";
+        tiles += "#..........#.g.#";
+        tiles += "#...########...#";
+        tiles += "#...#......#...#";
+        tiles += "#...#.a..g.#.c.#";
+        tiles += "#...#......A...#";
+        tiles += "#...#.c....#...#";
+        tiles += "#...G...##G#####";
+        tiles += "#...#...#......#";
+        tiles += "#...#...#...c..#";
+        tiles += "#...#...#..###.#";
+        tiles += "#.c.#...#..XE#.#";
+        tiles += "#...#...#..###.#";
+        tiles += "#...#####......#";
+        tiles += "################";
+
+        String entities = "";
+        entities +="................";
+        entities +="................";
+        entities +="................";
+        entities +="................";
+        entities +="................";
+        entities +="................";
+        entities +="................";
+        entities +="................";
+        entities +="................";
+        entities +="................";
+        entities +="................";
+        entities +="................";
+        entities +="......C.........";
+        entities +="................";
+        entities +="................";
+        entities +="................";
+
+        LevelHandler levelOne = new LevelHandler(16, 16, 1, entities, tiles);
+        File fileToSave = new File("level1.xml");
+        new ConcretePersister().saveLevel(1, levelOne);
+    }
+
+    @Test
+    public void loadSavedLevel() throws FileNotFoundException, PersistException {
+        InputStream is = getClass().getResourceAsStream("/nz/ac/vuw/ecs/swen225/gp21/persistency/levels/level1.xml");
+        File fileToLoad = new File("levels/level1.xml");
+        XMLParser parser = new XMLParser(new XmlMapper());
+        LevelHandler levelHandler = new XMLParser(new XmlMapper()).load(is, LevelHandler.class);
+        Level loadedLevel = LevelHandler.toLevel(levelHandler);
+    }
+}
