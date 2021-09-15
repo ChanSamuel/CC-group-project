f02110c17918c4369324e9597ee28a8c648e241f
lucygoodie <79559998+lucygoodie@users.noreply.github.com>
2021-09-15 16:34:45 +1200

    Create LevelHandler.java
    In progress class to act as a middle man between Level and Persistency

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/persistency/LevelHandler.java
@@ -0,0 +1,21 @@
+package nz.ac.vuw.ecs.swen225.gp21.persistency;
+
+import nz.ac.vuw.ecs.swen225.gp21.domain.Level;
+
+public class LevelHandler {
+
+    private int rows, cols, levelNumber;
+    private String terrainLayout, entityLayout;
+
+    public LevelHandler(int rows, int cols, int levelNumber, String terrainLayout, String entityLayout) {
+        this.rows = rows;
+        this.cols = cols;
+        this.levelNumber = levelNumber;
+        this.terrainLayout = terrainLayout;
+        this.entityLayout = entityLayout;
+    }
+
+    public static Level toLevel(LevelHandler levelHandler) {
+        return null;
+    }
+}
