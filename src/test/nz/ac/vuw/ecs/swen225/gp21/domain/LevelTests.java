f028aa05c10c2e85dd695220391fde4a5cf0840e
Ben Sanson <bsanson90@gmail.com>
2021-09-29 14:00:04 +1300

    Added tests for level type

---
+++ src/test/nz/ac/vuw/ecs/swen225/gp21/domain/LevelTests.java
@@ -0,0 +1,56 @@
+package test.nz.ac.vuw.ecs.swen225.gp21.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import nz.ac.vuw.ecs.swen225.gp21.domain.Coord;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Level;
+
+class LevelTests {
+
+  @Test
+  void testLevelInvalidArgs() {
+    // test invalid args 3x
+    assertThrows(IllegalArgumentException.class, () -> {
+      Level l = new Level(-1, -1, "", "", "null");
+    });
+
+    assertThrows(IllegalArgumentException.class, () -> {
+      Level l = new Level(10, -1, "", "", "null");
+    });
+
+    assertThrows(IllegalArgumentException.class, () -> {
+      Level l = new Level(0, 10, "", "", "null");
+    });
+
+    assertThrows(IllegalArgumentException.class, () -> {
+      Level l = new Level(3, 3, "...", "..", "null");
+    });
+
+    assertThrows(IllegalArgumentException.class, () -> {
+      Level l = new Level(10, 10, ".", ".", "null");
+    });
+
+    assertThrows(IllegalArgumentException.class, () -> {
+      Level l = new Level(1, 1, " ", ".", "null");
+    });
+
+    assertThrows(IllegalArgumentException.class, () -> {
+      Level l = new Level(1, 1, ".", " ", "null");
+    });
+
+    assertDoesNotThrow(() -> {
+      Level l = new Level(1, 1, ".", ".", "null");
+    });
+  }
+
+  @Test
+  void testTerrainAt() {
+    assertThrows(RuntimeException.class, () -> {
+      Level l = new Level(2, 2, "1...", "....", "testLevel");
+      l.terrainAt(new Coord(0, 0));
+    });
+  }
+
+}
