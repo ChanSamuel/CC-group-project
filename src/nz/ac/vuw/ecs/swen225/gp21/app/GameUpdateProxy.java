5660fdb7055d4f879aed7af4030b76d2718b636c
Samuel Chan <83254763+ChanSamuel@users.noreply.github.com>
2021-09-30 18:50:34 +1300

    Refactoring Recorder and Adding default constructor and getters for Persistency to use.

--- src/nz/ac/vuw/ecs/swen225/gp21/app/GameUpdateProxy.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/app/GameUpdateProxy.java
@@ -1,13 +1,21 @@
 package nz.ac.vuw.ecs.swen225.gp21.app;
 
 import nz.ac.vuw.ecs.swen225.gp21.domain.GameEvent;
-import nz.ac.vuw.ecs.swen225.gp21.domain.World;
 import nz.ac.vuw.ecs.swen225.gp21.recorder.GameUpdate;
 
 public class GameUpdateProxy implements GameUpdate {
 	
 	private GameEvent ge;
 	
+	
+	public GameUpdateProxy() {
+		
+	}
+	
+	public GameEvent getGe() {
+		return ge;
+	}
+
 	public GameUpdateProxy(GameEvent ge) {
 		this.ge = ge;
 	}
--- src/nz/ac/vuw/ecs/swen225/gp21/recorder/GameUpdate.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/recorder/GameUpdate.java
@@ -1,8 +1,11 @@
 package nz.ac.vuw.ecs.swen225.gp21.recorder;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 /**
  * Forces given objects to have the required data for Recorder
  */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
 public interface GameUpdate {
     /**
      * Returns a unique number that identifies on which game loop this command was executed.
